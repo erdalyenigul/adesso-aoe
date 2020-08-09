211bd001cad812081a7a02ea82978b32492ef355
erdal.yenigul <erdal.yenigul>
2020-08-09 15:11:15 +0300

    error page added

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    error
+  <div class="container error">
+    <span>Aradığınız sayfa bulunamadı.</span>
+    <img src="../assets/images/404.png" alt="">
   </div>
 </template>
 
